eb411012ba61685d2a04fb8fa1508854574c4c11
vin1112 <vkvijethkumar24@gmail.com>
2025-03-28 19:07:40 +0530

    added connection

--- admin/config.php
+++ admin/config.php
@@ -1,8 +1,11 @@
 <?php
-
-    $conn = mysqli_connect("localhost", "root", "", "solar_edge");
-    if(!$conn)
-    {
-        die ("Unable to connect database : ".mysqli_connect_error());
-    }
+$host = "metro.proxy.rlwy.net";  // Railway MySQL Host
+$port = "52356";                 // Railway MySQL Port
+$username = "root";               // Railway MySQL Username
+$password = "SGkaAIcqtrRTCGkuRAZTngQRDhLvAEgK"; // Railway MySQL Password
+$database = "railway";
+$conn = mysqli_connect($host, $username, $password, $database, $port);
+if (!$conn) {
+    die("Unable to connect database : " . mysqli_connect_error());
+}
 ?>
