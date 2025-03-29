5a640b597a35eead3866ebb97e6c547afa366034
vin1112 <vkvijethkumar24@gmail.com>
2025-03-29 11:00:33 +0530

    added connection2

--- admin/config.php
+++ admin/config.php
@@ -1,12 +1,10 @@
 <?php
-$host = "
-mysql.railway.internal";  // Railway MySQL Host
-$port = "
-3306";                 // Railway MySQL Port
+$host = "mysql.railway.internal";  // Railway MySQL Host
+$port = 3306;                 // Railway MySQL Port
 $username = "root";               // Railway MySQL Username
 $password = "SGkaAIcqtrRTCGkuRAZTngQRDhLvAEgK"; // Railway MySQL Password
 $database = "railway";
-$conn = mysqli_connect($host, $port, $username, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database, $port);
 if (!$conn) {
     die("Unable to connect database : " . mysqli_connect_error());
 }
