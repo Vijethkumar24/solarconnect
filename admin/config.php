ea834a10f55d486c0ca14842d6b6f1d6af066fdb
vin1112 <vkvijethkumar24@gmail.com>
2025-03-29 10:56:22 +0530

    updated DockerFile

--- admin/config.php
+++ admin/config.php
@@ -1,10 +1,12 @@
 <?php
-$host = "metro.proxy.rlwy.net";  // Railway MySQL Host
-$port = "52356";                 // Railway MySQL Port
+$host = "
+mysql.railway.internal";  // Railway MySQL Host
+$port = "
+3306";                 // Railway MySQL Port
 $username = "root";               // Railway MySQL Username
 $password = "SGkaAIcqtrRTCGkuRAZTngQRDhLvAEgK"; // Railway MySQL Password
 $database = "railway";
-$conn = mysqli_connect($host, $username, $password, $database);
+$conn = mysqli_connect($host, $port, $username, $password, $database);
 if (!$conn) {
     die("Unable to connect database : " . mysqli_connect_error());
 }
