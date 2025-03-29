f282872a20c4102ecad171f5a626711e45fde904
vin1112 <vkvijethkumar24@gmail.com>
2025-03-29 11:15:14 +0530

    removed hardcoded conn values

--- config/connection.php
+++ config/connection.php
@@ -1,9 +1,10 @@
 <?php
-$host = getenv('MYSQLHOST');
-$port = (int) getenv('MYSQLPORT');
-$username = getenv('MYSQLUSER');
-$password = getenv('MYSQLPASSWORD');
-$database = getenv('MYSQLDATABASE');
+$host = $_ENV['MYSQLHOST'] ?? 'localhost';
+$port = (int) ($_ENV['MYSQLPORT'] ?? 3306);
+$username = $_ENV['MYSQLUSER'] ?? 'root';
+$password = $_ENV['MYSQLPASSWORD'] ?? '';
+$database = $_ENV['MYSQLDATABASE'] ?? 'test_db';
+
 
 $conn = mysqli_connect($host, $username, $password, $database, $port);
 
