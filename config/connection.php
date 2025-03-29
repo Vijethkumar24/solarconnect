811d1e56ef11e929141010ffe94578958779ef20
vin1112 <vkvijethkumar24@gmail.com>
2025-03-29 10:46:20 +0530

    updated conn

--- config/connection.php
+++ config/connection.php
@@ -4,7 +4,7 @@ $port = "52356";                 // Railway MySQL Port
 $username = "root";               // Railway MySQL Username
 $password = "SGkaAIcqtrRTCGkuRAZTngQRDhLvAEgK"; // Railway MySQL Password
 $database = "railway";
-$conn = mysqli_connect($host . ':' . $port, $username, $password, $database);
+$conn = mysqli_connect($host, $username, $password, $database);
 
 if (!$conn) {
     die("Unable to connect database : " . mysqli_connect_error());
